236aaea6b1b302d13850e580bad9aed360cae4f6
skk <skkdetroit@gmail.com>
2021-10-09 18:34:54 -0400

    removed store provider and moved tapp.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// Connecting the react/redux store
-import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-import store from "./utils/store";
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-  <Provider store = {store} >
     <App />
-  </Provider>
-
   </React.StrictMode>,
   document.getElementById('root')
 );
