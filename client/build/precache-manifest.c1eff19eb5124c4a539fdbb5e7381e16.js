2da0dae24520ab6e5e8e2556cca07fef8cfdfb6a
skk <skkdetroit@gmail.com>
2021-10-12 23:07:07 -0400

    updated dependencies

---
+++ client/build/precache-manifest.c1eff19eb5124c4a539fdbb5e7381e16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c1dadb9dd3d3f3e043ce3f8bbd19f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3be54dfeba6355052dc9",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "79a84aa4cd9a8c93f5ff",
+    "url": "/static/js/2.f9d92c62.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.f9d92c62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3be54dfeba6355052dc9",
+    "url": "/static/js/main.46a4b46c.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
